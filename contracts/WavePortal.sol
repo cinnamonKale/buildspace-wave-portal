8a7aa2f2c818ffd693dc9db37d79393424df4627
Kyle Simmonds <6350880+cinnamonkale@users.noreply.github.com>
2021-11-26 18:50:34 -0600

    Add fields to wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,13 +7,31 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Smart contracts, uh, find a way");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    constructor() {
+        console.log("I AM SMART CONTRACT. POG.");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
